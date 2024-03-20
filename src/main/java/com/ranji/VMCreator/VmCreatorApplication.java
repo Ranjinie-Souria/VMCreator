f4aa712dd34a728c3aaec94fa0ab411e26383562
Ranjinie Vingadassamy <ranjinie.souria@gmail.com>
2024-03-20 23:09:19 +0100

    initial commit

---
+++ src/main/java/com/ranji/VMCreator/VmCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.ranji.VMCreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranji/VMCreator/VmCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranji.VMCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
